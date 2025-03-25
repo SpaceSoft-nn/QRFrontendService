a34c90b761d992640fbbcb333f592fa6e706f617
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2025-03-25 17:25:11 +0300

    feat(app): added toaster provider

--- src/app/providers/app-providers.tsx
+++ src/app/providers/app-providers.tsx
@@ -1,4 +1,5 @@
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from '@/shared/ui'
 import { AppRouter } from '../router/app-router'
 import { ThemeProvider } from './theme/theme-provider'
 
@@ -6,6 +7,7 @@ export function AppProviders() {
 	return (
 		<ThemeProvider>
 			<RouterProvider router={AppRouter} />
+			<Toaster />
 		</ThemeProvider>
 	)
 }
