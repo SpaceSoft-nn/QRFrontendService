208b3f87ad1371bd0a9c029811dea97626381e10
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2025-04-02 01:59:28 +0300

    feat(pages): fixed not found page link issue

--- src/Pages/home/HomePage.tsx
+++ src/Pages/home/HomePage.tsx
@@ -1,47 +1,14 @@
-import { useForm } from 'react-hook-form'
-import { FormProvider } from 'react-hook-form'
-import { User } from 'lucide-react'
-import { FormCheckbox, FormCommand, FormDateInput, FormInput, FormSelect } from '@/shared/ui/Forms'
-
-const items = [
-	{
-		value: '1',
-		label: 'Item 1',
-		description: 'Item 1 description',
-		icon: User
-	},
-	{
-		value: '2',
-		label: 'Item 2',
-		description: 'Item 2 description',
-		icon: User
-	},
-	{
-		value: '3',
-		label: 'Item 3',
-		description: 'Item 3 description',
-		icon: User
-	},
-	{
-		value: '4',
-		label: 'Item 4',
-		description: 'Item 4 description',
-		icon: User
-	}
-]
+import { Skeleton } from '@/shared/ui'
 
 export const HomePage = () => {
-	const form = useForm()
-
 	return (
 		<section className='grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3'>
-			<FormProvider {...form}>
-				<FormCommand items={items} name='item' />
-				<FormDateInput name='date' />
-				<FormInput name='input' placeholder='Введите текст' />
-				<FormSelect name='select' items={items} placeholder='Select' />
-				<FormCheckbox name='checkbox' label='Checkbox' />
-			</FormProvider>
+			<Skeleton className='h-[300px] w-full' />
+			<Skeleton className='h-[300px] w-full' />
+			<Skeleton className='h-[300px] w-full' />
+			<Skeleton className='h-[300px] w-full' />
+			<Skeleton className='h-[300px] w-full' />
+			<Skeleton className='h-[300px] w-full' />
 		</section>
 	)
 }
--- src/Pages/not-found/NotFoundPage.tsx
+++ src/Pages/not-found/NotFoundPage.tsx
@@ -6,20 +6,17 @@ import { urls } from '@/shared/config'
 export const NotFoundPage = () => {
 	return (
 		<div className='flex h-svh items-center justify-center'>
-			<Card className='max-w-md mx-auto'>
+			<Card className='max-w-sm mx-auto border-none'>
 				<CardHeader>
 					<CardTitle className='text-6xl font-bold text-center'>404</CardTitle>
-					<CardDescription>
+					<CardDescription className='text-center'>
 						Извините, но страница, которую вы ищете, не существует или была перемещена.
 					</CardDescription>
 				</CardHeader>
-				<CardFooter>
-					<Button asChild className='w-full'>
-						<Link to={urls.dashboard.main}>
-							<ExternalLinkIcon className='h-4 w-4' />
-							Вернуться на главную
-						</Link>
-					</Button>
+				<CardFooter className='flex justify-center'>
+					<Link to={urls.dashboard.main}>
+						<Button icon={ExternalLinkIcon}>Вернуться на главную</Button>
+					</Link>
 				</CardFooter>
 			</Card>
 		</div>
