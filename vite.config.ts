4142ca636136149c850c21aa32d9673503fc21e5
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2025-04-02 02:46:07 +0300

    chore: removed unused deps, added manual chunks for build

--- vite.config.ts
+++ vite.config.ts
@@ -23,5 +23,37 @@ export default defineConfig({
 	},
 	server: {
 		port: 4000
+	},
+	build: {
+		chunkSizeWarningLimit: 500,
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					'vendor-react': ['react', 'react-dom'],
+					'vendor-ui': [
+						'@radix-ui/react-accordion',
+						'@radix-ui/react-checkbox',
+						'@radix-ui/react-dialog',
+						'@radix-ui/react-dropdown-menu',
+						'@radix-ui/react-label',
+						'@radix-ui/react-popover',
+						'@radix-ui/react-scroll-area',
+						'@radix-ui/react-select',
+						'@radix-ui/react-separator',
+						'@radix-ui/react-slot',
+						'@radix-ui/react-switch',
+						'@radix-ui/react-tabs',
+						'@radix-ui/react-toast',
+						'@radix-ui/react-tooltip',
+						'shadcn',
+						'lucide-react',
+						'react-icons',
+						'react-table',
+						'react-day-picker'
+					],
+					'vendor-gql': ['@apollo/client', 'graphql']
+				}
+			}
+		}
 	}
 })
