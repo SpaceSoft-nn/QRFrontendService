43ab0c87579b21ea65d22d5cdd54f7cc35542f3d
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2025-04-02 03:13:48 +0300

    docker build fix: [vite:load-fallback] Could not load /app/src/pages/not-found/NotFoundPage

--- src/app/router/app-router.tsx
+++ src/app/router/app-router.tsx
@@ -1,15 +1,9 @@
-import { lazy } from 'react'
 import { createBrowserRouter } from 'react-router-dom'
+import { NotFoundPage, SignInPage, SignUpPage } from '@/pages'
+import { HomePage } from '@/pages/home'
 import { MainLayout } from './layouts/main'
 import { ProtectedRoute } from './protected-route'
 import { urls } from '@/shared/config'
-
-const NotFoundPage = lazy(() =>
-	import('@/pages/not-found/NotFoundPage').then(module => ({ default: module.NotFoundPage }))
-)
-const SignInPage = lazy(() => import('@/pages/auth/SignInPage').then(module => ({ default: module.SignInPage })))
-const SignUpPage = lazy(() => import('@/pages/auth/SignUpPage').then(module => ({ default: module.SignUpPage })))
-const HomePage = lazy(() => import('@/pages/home/HomePage').then(module => ({ default: module.HomePage })))
 
 export const AppRouter = createBrowserRouter([
 	{
