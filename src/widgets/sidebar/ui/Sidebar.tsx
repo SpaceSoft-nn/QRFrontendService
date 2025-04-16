9e7908825d302b348cdf56639402963c12c55d30
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2025-04-16 03:22:12 +0300

    feat(sidebar): removed mock for organization switcher

--- src/widgets/sidebar/ui/Sidebar.tsx
+++ src/widgets/sidebar/ui/Sidebar.tsx
@@ -1,14 +1,8 @@
-import { NavUser } from '@/features/nav-user'
-import { OrganizationSwitcher } from '@/features/organization-switcher'
+import { OrganizationSwitcher } from '@/features/organization'
+import { NavUser } from '@/features/user'
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader } from '@/shared/ui/Sidebar'
 import { NavBottom } from './navs/NavBottom'
 import { NavMain } from './navs/NavMain'
-
-const organizations = [
-	{ id: '1', name: 'ООО «Рога и копыта»', role: 'Администратор' },
-	{ id: '2', name: 'ООО «Копыта и рога»', role: 'Пользователь' },
-	{ id: '3', name: 'ООО «Агропром»', role: 'Пользователь' }
-]
 
 type AppSidebarProps = React.ComponentProps<typeof Sidebar>
 
@@ -16,7 +10,7 @@ export const AppSidebar: React.FC<AppSidebarProps> = ({ ...props }) => {
 	return (
 		<Sidebar collapsible='icon' {...props}>
 			<SidebarHeader>
-				<OrganizationSwitcher organizations={organizations} />
+				<OrganizationSwitcher />
 			</SidebarHeader>
 			<SidebarContent>
 				<NavMain />
