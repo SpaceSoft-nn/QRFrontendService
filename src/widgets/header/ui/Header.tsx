3284be0889d14df3b7896197d329426b7b968da6
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2025-04-22 01:31:02 +0300

    feat(header): added sticky position

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -5,7 +5,7 @@ import { SidebarTrigger } from '@/shared/ui/Sidebar'
 
 export const Header: React.FC = () => {
 	return (
-		<header className='flex h-16 shrink-0 justify-between items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12'>
+		<header className='flex h-16 shrink-0 sticky bg-background/80 backdrop-blur-sm top-0 z-50 justify-between items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12'>
 			<div className='flex items-center gap-2 w-full'>
 				<SidebarTrigger />
 				<Separator orientation='vertical' className='h-4' />
