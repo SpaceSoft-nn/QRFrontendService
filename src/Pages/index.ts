bd38e840e9f45e4ff7f9781fb4fa4196d3610aca
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2025-04-26 02:13:08 +0300

    pages exports

--- src/Pages/index.ts
+++ src/Pages/index.ts
@@ -1,7 +1,9 @@
 export * from './organizations'
 export * from './workspaces'
 export * from './members'
+export * from './integrations'
+
 export * from './HomePage'
+export * from './ProfilePage'
 
 export * from './NotFoundPage'
-export * from './ProfilePage'
