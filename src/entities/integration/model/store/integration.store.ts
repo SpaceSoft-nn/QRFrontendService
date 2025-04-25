ee96164ddf72084cf62e7c44a2ee62e7d2320adf
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2025-04-26 00:01:26 +0300

    feat(integration): created gql queries & mutations, created store

---
+++ src/entities/integration/model/store/integration.store.ts
@@ -0,0 +1,77 @@
+import { makeAutoObservable, runInAction } from 'mobx'
+import { apolloClient } from '@/shared/api'
+import { DriverInfo, DriverInfoInput, Mutation, Query } from '@/shared/api/graphql'
+import { toast } from '@/shared/lib'
+import { CREATE_INTEGRATION_MUTATION, GET_INTEGRATION_QUERY } from '../../gql'
+
+class IntegrationStore {
+	intergations: DriverInfo[] = []
+	loading: boolean = false
+	error: string | null = null
+
+	constructor() {
+		makeAutoObservable(this)
+	}
+
+	async getIntegation(id: string) {
+		try {
+			this.loading = true
+			this.error = null
+
+			const { data } = await apolloClient.query<Pick<Query, 'driverInfoById'>>({
+				query: GET_INTEGRATION_QUERY,
+				variables: { id }
+			})
+
+			runInAction(() => {
+				if (data?.driverInfoById) {
+					this.intergations = [...this.intergations, data.driverInfoById]
+				}
+			})
+		} catch (error) {
+			this.error = error instanceof Error ? error.message : 'Произошла ошибка при получении интеграции'
+			toast({
+				title: 'Ошибка при получении интеграции',
+				variant: 'destructive',
+				description: this.error
+			})
+			console.error('[getIntegation] error: ', error)
+		} finally {
+			runInAction(() => {
+				this.loading = false
+			})
+		}
+	}
+
+	async createIntergration(input: DriverInfoInput) {
+		try {
+			this.loading = true
+			this.error = null
+
+			const { data } = await apolloClient.mutate<Pick<Mutation, 'createDriverInfo'>>({
+				mutation: CREATE_INTEGRATION_MUTATION,
+				variables: { input }
+			})
+
+			runInAction(() => {
+				if (data?.createDriverInfo) {
+					this.intergations = [...this.intergations, data.createDriverInfo]
+				}
+			})
+		} catch (error) {
+			console.error('[createIntergration] error: ', error)
+			this.error = error instanceof Error ? error.message : 'Произошла ошибка при создании интеграции'
+			toast({
+				title: 'Ошибка при создании интеграции',
+				variant: 'destructive',
+				description: this.error
+			})
+		} finally {
+			runInAction(() => {
+				this.loading = false
+			})
+		}
+	}
+}
+
+export const integrationStore = new IntegrationStore()
