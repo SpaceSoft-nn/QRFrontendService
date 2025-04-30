dc4f8a53aa9ec34bb5a6c315a37008e7896601bb
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2025-04-30 13:32:02 +0300

    feat(api): edited 401 handle

--- src/shared/api/apollo.ts
+++ src/shared/api/apollo.ts
@@ -1,4 +1,4 @@
-import { ApolloClient, createHttpLink, from, InMemoryCache, Operation } from '@apollo/client'
+import { ApolloClient, createHttpLink, from, fromPromise, InMemoryCache, Observable } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
 import { onError } from '@apollo/client/link/error'
 import { authStore } from '@/features/auth'
@@ -8,7 +8,8 @@ const httpLink = createHttpLink({
 	uri: import.meta.env.VITE_GQL_URL,
 	credentials: 'include',
 	fetchOptions: {
-		timeout: 30000
+		timeout: 30000,
+		credentials: 'include'
 	}
 })
 
@@ -22,20 +23,6 @@ const authLink = setContext((_, { headers }) => {
 	}
 })
 
-const handleUnauthenticated = async (operation: Operation) => {
-	try {
-		const success = await authStore.refreshToken()
-		if (!success) {
-			authStore.logout()
-			window.location.href = urls.auth.login
-		}
-	} catch (error) {
-		console.error(error)
-		authStore.logout()
-		window.location.href = urls.auth.login
-	}
-}
-
 const handleGraphQLError = (error: any) => {
 	console.error(`[GraphQL error]: Message: ${error.message}, Location: ${error.locations}, Path: ${error.path}`)
 }
@@ -47,9 +34,31 @@ const handleNetworkError = (error: any) => {
 const errorLink = onError(({ graphQLErrors, networkError, operation, forward }) => {
 	if (graphQLErrors) {
 		for (const err of graphQLErrors) {
-			if (err.message.includes('Unauthenticated')) {
-				handleUnauthenticated(operation)
-				return
+			if (err.message.includes('Unauthenticated.')) {
+				return fromPromise(
+					authStore.refreshToken().then(success => {
+						if (success) {
+							const oldHeaders = operation.getContext().headers
+							const token = localStorage.getItem('token')
+							operation.setContext({
+								headers: {
+									...oldHeaders,
+									authorization: token ? `Bearer ${token}` : ''
+								}
+							})
+							return true
+						} else {
+							authStore.logout()
+							window.location.href = urls.auth.login
+							return false
+						}
+					})
+				).flatMap(success => {
+					if (success) {
+						return forward(operation)
+					}
+					return Observable.of() // пустой Observable, если неуспех
+				})
 			}
 			handleGraphQLError(err)
 		}
