9f960a603599cbfc6e9c2d93afdef4c1238c5fa1
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2025-05-02 18:02:41 +0300

    fix(organization): removed authorized user from organization members options

--- src/entities/organization/model/store/organization.store.ts
+++ src/entities/organization/model/store/organization.store.ts
@@ -1,7 +1,7 @@
 import { makeAutoObservable, reaction, runInAction } from 'mobx'
 import { authStore } from '@/features/auth'
 import { PartySuggestionsValue } from '@/entities/organization'
-import { userStore } from '@/entities/user'
+import { formatFullName, userStore } from '@/entities/user'
 import { Organization, User, UserCreate } from '@/shared/api/graphql'
 import { capitalizeFullName, toast } from '@/shared/lib'
 import { organizationApi } from '../../api/organization.api'
@@ -24,8 +24,10 @@ class OrganizationStore {
 	}
 
 	membersOptions = () => {
-		return this.organizationMembers.map(member => ({
-			label: `${member.last_name} ${member.first_name} ${member.father_name}`,
+		const members = this.organizationMembers.filter(member => member.id !== userStore.user?.id)
+
+		return members.map(member => ({
+			label: formatFullName(member, { initials: true }),
 			description: member?.email || member?.phone || null,
 			value: member.id
 		}))
